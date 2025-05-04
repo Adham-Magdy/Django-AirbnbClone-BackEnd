52d153d8d6b1a612c36c1a085116fa4db98c27e0
Adham-Magdy <adhammagdy349@gmail.com>
2025-05-04 16:31:33 +0300

    implement post api method for property form

--- property/api.py
+++ property/api.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 
 from rest_framework.decorators import api_view,permission_classes,authentication_classes
+from property.forms import PropertyForm
 from .models import Property
 from .serializers import PropertySerializer
 
@@ -14,3 +15,22 @@ def getProperties(request):
     return JsonResponse({
         'data':serializers.data
     })
+
+# POST Property Data
+@api_view(['POST','FILES'])
+def create_property(request):
+    form = PropertyForm(request.POST,request.FILES)
+    
+    # check form validation
+    if form.is_valid():
+        property = form.save(commit=False)
+        property.landlord = request.user
+        property.save()
+        
+        return JsonResponse({'success':True})
+    
+    else:
+        print("error:",form.errors,form.non_field_errors)
+        return JsonResponse({"errors":form.errors.as_json()})
+        
+    
