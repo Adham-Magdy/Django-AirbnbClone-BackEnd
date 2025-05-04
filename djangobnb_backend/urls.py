2cc4cb07cab7b71ec789d120ff533a018278889f
Adham-Magdy <adhammagdy349@gmail.com>
2025-05-04 15:24:06 +0300

    feat: implement property form service

--- djangobnb_backend/urls.py
+++ djangobnb_backend/urls.py
@@ -7,5 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/properties',include('property.url')),
-    path('api/auth/',include('user_account.urls'))
+    path('api/auth/',include('user_account.url'))
 ]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
