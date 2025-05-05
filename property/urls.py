0826394142c0783984ae61d78e8cf97f9b0a005b
Adham-Magdy <adhammagdy349@gmail.com>
2025-05-05 13:31:49 +0300

    adding url with id param

--- property/urls.py
+++ property/urls.py
@@ -5,5 +5,7 @@ from . import api
 urlpatterns =[
     path('',api.getProperties,name="api_properties_list"),
     path('create/', api.create_property, name='api_create_property'),
+    path('<uuid:pk>/', api.property_details, name='api_property_detail'),
+
 
 ]
