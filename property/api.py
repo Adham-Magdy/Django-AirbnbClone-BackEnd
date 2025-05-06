45a4aa6dbc077b3dc3da9882304131cb2862793f
Adham-Magdy <adhammagdy349@gmail.com>
2025-05-06 16:05:09 +0300

    implement reservation db model && reservation post api method

--- property/api.py
+++ property/api.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view,permission_classes,authentication_classes
 from property.forms import PropertyForm
-from .models import Property
+from .models import Property, Reservation
 from .serializers import PropertySerializer,PropertyDetailSerializer
 
 @api_view(['GET'])
@@ -44,3 +44,31 @@ def create_property(request):
         return JsonResponse({"errors":form.errors.as_json()})
         
     
+# POST property reservation
+@api_view(['POST'])
+def book_property(request,pk):
+    try:
+        start_date = request.POST.get('start_date','')
+        end_date = request.POST.get('end_date','')
+        number_of_nights= request.POST.get('number_of_nights','')
+        guests = request.POST.get('guests','')
+        total_price = request.POST.get('total_price','')
+        
+        # get property based on his id
+        property = Property.objects.get(pk=pk)
+        
+        # create reservation
+        Reservation.objects.create(
+            property=property,
+            start_date=start_date,
+            end_date=end_date,
+            number_of_nights = number_of_nights,
+            guests = guests,
+            total_price = total_price,
+            created_by= request.user
+        )
+        
+    except Exception as e:
+        print(e)
+        
+        return JsonResponse({'success':False})
--- property/urls.py
+++ property/urls.py
@@ -6,6 +6,6 @@ urlpatterns =[
     path('',api.getProperties,name="api_properties_list"),
     path('create/', api.create_property, name='api_create_property'),
     path('<uuid:pk>/', api.property_details, name='api_property_detail'),
-
+    path('<uuid:pk>/book/',api.book_property,name="api_book_property")
 
 ]
